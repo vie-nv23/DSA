9f1a81cbb9d06c9b360e6b321b76f2be44d03642
Đanz <huy@HuyKaiDu.local>
2026-05-28 10:23:33 +0700

    toiuu

--- baitaptuan6.cpp
+++ baitaptuan6.cpp
@@ -2,17 +2,21 @@
 #include <string>
 
 using namespace std;
+
 struct File
 {
     string tenFile;
     double kichThuoc;
     string thoiGian;
 };
+
 struct Node
 {
     File data;
     Node* next;
-};Node* taoNode(File x)
+};
+
+Node* taoNode(File x)
 {
     Node* p = new Node;
 
@@ -23,25 +27,79 @@ struct Node
     p->next = NULL;
 
     return p;
-}void chenCuoi(Node*& head, File x)
+}
+
+void chenTheoThoiGian(Node*& head, File x)
 {
     Node* p = taoNode(x);
 
+    // Danh sach rong
     if (head == NULL)
     {
         head = p;
         return;
     }
 
+    // Chen vao dau
+    if (x.thoiGian < head->data.thoiGian)
+    {
+        p->next = head;
+        head = p;
+        return;
+    }
+
+    // Tim vi tri chen
     Node* temp = head;
 
-    while (temp->next != NULL)
+    while (temp->next != NULL &&
+           temp->next->data.thoiGian < x.thoiGian)
     {
         temp = temp->next;
     }
 
+    // Chen vao giua hoac cuoi
+    p->next = temp->next;
     temp->next = p;
-}void hienThi(Node* head)
+}
+
+double tinhTongKichThuoc(Node* head)
+{
+    double tong = 0;
+
+    Node* p = head;
+
+    while (p != NULL)
+    {
+        tong += p->data.kichThuoc;
+        p = p->next;
+    }
+
+    return tong;
+}
+
+Node* timFileNhoNhat(Node* head)
+{
+    if (head == NULL)
+        return NULL;
+
+    Node* minNode = head;
+    Node* p = head->next;
+
+    while (p != NULL)
+    {
+        if (p->data.kichThuoc <
+            minNode->data.kichThuoc)
+        {
+            minNode = p;
+        }
+
+        p = p->next;
+    }
+
+    return minNode;
+}
+
+void hienThi(Node* head)
 {
     Node* p = head;
 
@@ -65,7 +123,9 @@ struct Node
 
         p = p->next;
     }
-}int main()
+}
+
+int main()
 {
     Node* head = NULL;
 
@@ -83,10 +143,29 @@ struct Node
         "2026-05-28 11:00"
     };
 
-    chenCuoi(head, f1);
-    chenCuoi(head, f2);
+    File f3 =
+    {
+        "Video.mp4",
+        200,
+        "2026-05-28 10:45"
+    };
+
+    chenTheoThoiGian(head, f1);
+    chenTheoThoiGian(head, f2);
+    chenTheoThoiGian(head, f3);
 
     hienThi(head);
+
+    cout << "Tong kich thuoc: "
+         << tinhTongKichThuoc(head)
+         << " MB"
+         << endl;
+
+    Node* minFile = timFileNhoNhat(head);
+
+    cout << "File nho nhat: "
+         << minFile->data.tenFile
+         << endl;
 
     return 0;
 }
