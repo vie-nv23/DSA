34d93e85328a40716ca1d067b55c0dc67b6da361
Đanz <huy@HuyKaiDu.local>
2026-05-28 17:26:45 +0700

    cộng hai đa thức bằng ds tuần tự

--- baitaptuan7.cpp
+++ baitaptuan7.cpp
@@ -49,15 +49,92 @@ void hienThiDaThuc(DaThuc p)
 
     cout << endl;
 }
+DaThuc congDaThuc(DaThuc p, DaThuc q)
+{
+    DaThuc r;
+    r.n = 0;
 
+    int i = 0;
+    int j = 0;
+
+    while (i < p.n && j < q.n)
+    {
+        // Cung so mu
+        if (p.a[i].SoMu == q.a[j].SoMu)
+        {
+            r.a[r.n].HeSo =
+                p.a[i].HeSo + q.a[j].HeSo;
+
+            r.a[r.n].SoMu =
+                p.a[i].SoMu;
+
+            r.n++;
+            i++;
+            j++;
+        }
+
+        // P co so mu lon hon
+        else if (p.a[i].SoMu >
+                 q.a[j].SoMu)
+        {
+            r.a[r.n] = p.a[i];
+
+            r.n++;
+            i++;
+        }
+
+        // Q co so mu lon hon
+        else
+        {
+            r.a[r.n] = q.a[j];
+
+            r.n++;
+            j++;
+        }
+    }
+
+    // Them phan con lai cua P
+    while (i < p.n)
+    {
+        r.a[r.n] = p.a[i];
+
+        r.n++;
+        i++;
+    }
+
+    // Them phan con lai cua Q
+    while (j < q.n)
+    {
+        r.a[r.n] = q.a[j];
+
+        r.n++;
+        j++;
+    }
+
+    return r;
+}
 int main()
 {
     DaThuc p;
+    DaThuc q;
+    DaThuc r;
 
+    cout << "Nhap da thuc P:" << endl;
     nhapDaThuc(p);
 
-    cout << "Da thuc vua nhap: ";
+    cout << "Nhap da thuc Q:" << endl;
+    nhapDaThuc(q);
+
+    cout << "Da thuc P: ";
     hienThiDaThuc(p);
+
+    cout << "Da thuc Q: ";
+    hienThiDaThuc(q);
+
+    r = congDaThuc(p, q);
+
+    cout << "Tong hai da thuc: ";
+    hienThiDaThuc(r);
 
     return 0;
 }
