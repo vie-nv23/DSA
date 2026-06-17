d1b7412e278c68d83dc96a92564d9f9425727442
Đanz <huy@192.168.1.21>
2026-06-17 21:23:57 +0700

    tạo ds lket

--- baitaptuan15/dothigt.cpp
+++ baitaptuan15/dothigt.cpp
@@ -5,6 +5,47 @@ int q[100];
 
 int dau = 0;
 int cuoi = -1;
+struct Node
+{
+    int data;
+
+    Node* next;
+};
+
+Node* taoNode(int x)
+{
+    Node* p = new Node;
+
+    p->data = x;
+
+    p->next = NULL;
+
+    return p;
+}
+
+void themCuoi(Node*& head,
+              int x)
+{
+    Node* p =
+        taoNode(x);
+
+    if (head == NULL)
+    {
+        head = p;
+    }
+    else
+    {
+        Node* t =
+            head;
+
+        while (t->next != NULL)
+        {
+            t = t->next;
+        }
+
+        t->next = p;
+    }
+}
 void BFS(int ke[][11],
          string ten[])
 {
@@ -68,6 +109,14 @@ int main()
             "Hoa Binh"};
 
     int ke[11][11];
+    Node* dsKe[11];
+
+for (int i = 0;
+     i < 11;
+     i++)
+{
+    dsKe[i] = NULL;
+}
 
     for (int i = 0;
          i < 11;
@@ -120,6 +169,45 @@ int main()
     ke[5][4] = 1;
     ke[4][5] = 1;
 
+
+    themCuoi(dsKe[0], 1);
+    themCuoi(dsKe[1], 0);
+
+    themCuoi(dsKe[0], 2);
+    themCuoi(dsKe[2], 0);
+
+    themCuoi(dsKe[0], 10);
+    themCuoi(dsKe[10], 0);
+
+    themCuoi(dsKe[0], 9);
+    themCuoi(dsKe[9], 0);
+
+    themCuoi(dsKe[0], 8);
+    themCuoi(dsKe[8], 0);
+
+    themCuoi(dsKe[0], 7);
+    themCuoi(dsKe[7], 0);
+
+    themCuoi(dsKe[2], 3);
+    themCuoi(dsKe[3], 2);
+
+    themCuoi(dsKe[3], 1);
+    themCuoi(dsKe[1], 3);
+
+    themCuoi(dsKe[1], 4);
+    themCuoi(dsKe[4], 1);
+
+    themCuoi(dsKe[7], 6);
+    themCuoi(dsKe[6], 7);
+
+    themCuoi(dsKe[6], 5);
+    themCuoi(dsKe[5], 6);
+
+    themCuoi(dsKe[7], 5);
+    themCuoi(dsKe[5], 7);
+
+    themCuoi(dsKe[5], 4);
+    themCuoi(dsKe[4], 5);
     cout
         << "Danh sach tp:"
         << endl;
@@ -136,5 +224,35 @@ int main()
     }
     cout << "BFS: " << endl;
     BFS(ke, ten);
+    cout
+    << endl;
+
+cout
+    << "Danh sach lien ket:"
+    << endl;
+
+for (int i = 0;
+     i < 11;
+     i++)
+{
+    cout
+        << ten[i]
+        << ": ";
+
+    Node* p =
+        dsKe[i];
+
+    while (p != NULL)
+    {
+        cout
+            << ten[p->data]
+            << " ";
+
+        p = p->next;
+    }
+
+    cout
+        << endl;
+}
     return 0;
 }
